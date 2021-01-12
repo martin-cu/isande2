6baecba65f0de329ac5e3dac6ea4037a113e5574
y2saur <61381930+y2saur@users.noreply.github.com>
2021-01-12 22:52:39 +0800

    Revert "Merge branch 'sales' of https://github.com/martin-cu/isande2 into sales"
    This reverts commit fde21f502c41a5ac0163b98e689f7c12af766568, reversing
    changes made to 4df40a9816973f5faefd5bdb3b08ffda3852c2bf.

--- controllers/homeController.js
+++
@@ -1,9 +0,0 @@
-const homeModel = require('../models/homeModel');
-const js = require('../public/assets/js/session.js');
-
-exports.queryOverview = function(req, res){
-	var html_data = {};
-	html_data = js.init_session(html_data, req.session.authority, req.session.initials, req.session.username, 'home');
-	res.render('home', html_data);
-}
-

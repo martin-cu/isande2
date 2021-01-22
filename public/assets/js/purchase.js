52e1a49eb7a3938cfd604fc28a345cb19d00e5df
y2saur <61381930+y2saur@users.noreply.github.com>
2021-01-22 16:00:29 +0800

    fixed sales details

---
+++ public/assets/js/purchase.js
@@ -0,0 +1,29 @@
+function view_sale_details(type){
+
+}
+
+$(document).ready(function() {
+	$("#date").change(function(){
+		$.get("/get_price", {product : $("#purchase_product").val(), date : $("#date").val()}, function(result){
+			//update price in hbs
+			$("#product_price").html(result.price);
+		});
+	});
+
+	$("#p_qty").keyup(function(){
+		//update total
+		$.get("/get_price", {product : $("#purchase_product").val(), date : $("#date").val()}, function(result){
+			var total = result.price * $("#p_qty").val();
+			$("#total_amt").val(total);
+		});
+	});
+
+	$("#purchase_product").change(function(){
+		//update total
+		$.get("/get_price", {product : $("#purchase_product").val(), date : $("#date").val()}, function(result){
+			$("#product_price").html(result.price);
+			var total = result.price * $("#p_qty").val();
+			$("#total_amt").val(total);
+		});
+	});
+});

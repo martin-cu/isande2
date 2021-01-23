2cc8a7de04082a520124fc1970026ee0d4d877b7
y2saur <61381930+y2saur@users.noreply.github.com>
2021-01-23 21:56:29 +0800

    review and back create

--- public/assets/js/purchase.js
+++ public/assets/js/purchase.js
@@ -1,5 +1,97 @@
-function view_sale_details(type){
 
+function purchase_review(){
+
+	$("#date_review").text($("#date").val());
+	$("#date").hide();
+	$("#date_review").show();
+
+	$("#purchase_lo_review").text($("#purchase_lo").val());
+	$("#purchase_lo").hide();
+	$("#purchase_lo_review").show();
+
+	$("#purchase_so_review").text($("#purchase_so").val());
+	$("#purchase_so").hide();
+	$("#purchase_so_review").show();
+
+	$("#purchase_product_review").text($("#purchase_product option:selected").text());
+	$("#purchase_product").hide();
+	$("#purchase_product_review").show();
+
+	$("#p_qty_review").text($("#p_qty").val());
+	$("#p_qty").hide();
+	$("#p_qty_review").show();
+
+	$("#total_amt_review").text($("#total_amt").val());
+	$("#total_amt").hide();
+	$("#total_amt_review").show();
+}
+
+
+function review_fields(){
+	var status = true;
+	if(!$("#date").val()){
+		$("#date").css("border-color", "rgb(235, 64, 54)");
+		status = false;
+	}
+	else
+		$("#date").css("border-color", "");
+
+
+	if(!$("#purchase_lo").val()){
+		$("#purchase_lo").css("border-color", "rgb(235, 64, 54)");
+		status = false;
+	}
+	else
+		$("#purchase_lo").css("border-color", "");
+
+	if(!$("#purchase_so").val()){
+		$("#purchase_so").css("border-color", "rgb(235, 64, 54)");
+		status = false;
+	}
+	else
+		$("#purchase_so").css("border-color", "");
+
+	if(!$("#total_amt").val()){
+		$("#total_amt").css("border-color", "rgb(235, 64, 54)");
+		status = false;
+	}
+	else
+		$("#total_amt").css("border-color", "");
+
+	if(!$("#p_qty").val()){
+		$("#p_qty").css("border-color", "rgb(235, 64, 54)");
+		status = false;
+	}
+	else
+		$("#p_qty").css("border-color", "");
+
+	return status;
+}
+
+
+function back(){
+	$("#date").show();
+	$("#date_review").hide();
+
+	$("#purchase_lo").show();
+	$("#purchase_lo_review").hide();
+
+	$("#purchase_so").show();
+	$("#purchase_so_review").hide();
+
+	$("#purchase_product").show();
+	$("#purchase_product_review").hide();
+
+	$("#p_qty").show();
+	$("#p_qty_review").hide();
+
+	$("#total_amt").show();
+	$("#total_amt_review").hide();
+
+	$("#back").remove();
+	$("#submit").remove();
+
+	$("#p_next").show();
 }
 
 $(document).ready(function() {
@@ -10,20 +102,62 @@ $(document).ready(function() {
 		});
 	});
 
-	$("#qty").keyup(function(){
+	$("#p_qty").keyup(function(){
 		//update total
-		$.get("/get_price", {product : $("#purchase_product").val(), date : $("#date").val()}, function(result){
-			var total = result.price * $("#p_qty").val();
-			$("#total_amt").val(total);
-		});
+		if($("#date").val())
+			$.get("/get_price", {product : $("#purchase_product").val(), date : $("#date").val()}, function(result){
+				var total = result.price * $("#p_qty").val();
+				$("#total_amt").val(total);
+			});
 	});
 
 	$("#purchase_product").change(function(){
 		//update total
-		$.get("/get_price", {product : $("#purchase_product").val(), date : $("#date").val()}, function(result){
-			$("#product_price").html(result.price);
-			var total = result.price * $("#p_qty").val();
-			$("#total_amt").val(total);
-		});
+		if($("#date").val())
+			$.get("/get_price", {product : $("#purchase_product").val(), date : $("#date").val()}, function(result){
+				$("#product_price").html(result.price);
+				var total = result.price * $("#p_qty").val();
+				$("#total_amt").val(total);
+			});
 	});
+
+
+
+
+	$("#p_next").on("click", function(){
+		
+
+		var submit;
+		submit = document.createElement("button");
+		submit.setAttribute("class", "btn btn-primary active text-center float-right d-xl-flex justify-content-xl-end align-items-xl-start");
+		submit.setAttribute("id", "submit");
+		submit.setAttribute("type", "submit");
+		submit.setAttribute("form", "purchaseForm");
+		submit.innerHTML = "Create";
+
+		var back;
+		back = document.createElement("button");
+		back.setAttribute("class", "btn btn-primary active text-center float-left d-xl-flex justify-content-xl-end align-items-xl-start");
+		back.setAttribute("id", "back");
+		back.setAttribute("onclick", "back()");
+		back.setAttribute("style", "background-color:grey;");
+		back.innerHTML = "Back";
+		
+		if(review_fields()){
+			purchase_review();
+			$("#p_next").hide();
+			$(".card-body").append(submit);
+			$(".card-body").append(back);
+		}
+		else{
+			alert("Please fill up fields");
+		}
+		
+
+
+
+	});
+
+
+	
 });

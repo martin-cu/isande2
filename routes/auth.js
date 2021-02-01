f5951b7e5bf42a5f56f6940d8847aa0cf7aa2ed6
Martin Cu <martin_cu@dlsu.edu.ph>
2021-02-01 20:19:05 +0800

    Revert "setup access controls and fixed button hover color"
    This reverts commit b8abd94e23f1cc14da59db177bf2c405a81bbf13.

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ const deliveryController = require('../controllers/deliveryController');
 const reportController = require('../controllers/reportController');
 const inventoryController = require('../controllers/inventoryController');
 
-const { isPrivate, isAdmin, isSales, isPurchasing, isLogistics } = require('../middlewares/checkAuth');
+const { isPrivate, isAdmin, isSales, isPurchasing } = require('../middlewares/checkAuth');
 //Consistent Pages
 router.get('/login', (req, res) => {
   res.render('login', {});
@@ -30,43 +30,43 @@ router.get('/reports/:type/:product', isPrivate, reportController.viewSalesDetai
 //Inventory
 router.get('/inventory', isPrivate, inventoryController.getProductInventory);
 router.get('/product_catalogue', isPrivate, inventoryController.getProductCatalogue);
-router.get('/change_price', isPrivate, inventoryController.getProductName);
+router.get('/change_price', isAdmin, inventoryController.getProductName);
 router.post('/change_price', isPrivate, inventoryController.changeProductPrice);
-router.get('/manual_count', isPrivate, inventoryController.getProductNameForManualCount);
+router.get('/manual_count', isAdmin, inventoryController.getProductNameForManualCount);
 
 
 //Sales
-router.get('/create_sales', isSales, salesController.getSaleOrderForm);
-router.post('/create_sales', isSales, salesController.createSaleRecord);
-router.post('/void_sales/:dr', isSales, salesController.voidSales);
-router.get('/view_payments', isSales, salesController.getPaymentsPage);
-router.post('/submit_payment', isSales, salesController.postPaymentForm);
-router.get('/track_sale_orders', isSales, salesController.getTrackOrdersPage);
-router.get('/changeSaleCalendar', isSales, salesController.changeCalendar);
-router.get('/view_sales_records', isSales, salesController.getSalesRecords);
-router.get('/view_sales_details/:dr', isSales, salesController.viewSalesDetails);
+router.get('/create_sales', isPrivate, salesController.getSaleOrderForm);
+router.post('/create_sales', isPrivate, salesController.createSaleRecord);
+router.post('/void_sales/:dr', isPrivate, salesController.voidSales);
+router.get('/view_payments', isPrivate, salesController.getPaymentsPage);
+router.post('/submit_payment', isPrivate, salesController.postPaymentForm);
+router.get('/track_sale_orders', isPrivate, salesController.getTrackOrdersPage);
+router.get('/changeSaleCalendar', isPrivate, salesController.changeCalendar);
+router.get('/view_sales_records', isPrivate, salesController.getSalesRecords);
+router.get('/view_sales_details/:dr', isPrivate, salesController.viewSalesDetails);
 
 //Purchasing
-router.get('/track_purchase_orders', isPurchasing, purchaseController.getTrackOrdersPage);
-router.post('/void_purchases/:lo', isPurchasing, purchaseController.voidPurchase);
-router.get('/changePurchaseCalendar', isPurchasing, purchaseController.changeCalendar);
-router.get('/view_purchase_records', isPurchasing, purchaseController.getPurchaseRecords);
-router.get('/view_purchase_details/:lo', isPurchasing, purchaseController.viewPurchaseDetails);
+router.get('/track_purchase_orders', isPrivate, purchaseController.getTrackOrdersPage);
+router.post('/void_purchases/:lo', isPrivate, purchaseController.voidPurchase);
+router.get('/changePurchaseCalendar', isPrivate, purchaseController.changeCalendar);
+router.get('/view_purchase_records', isPrivate, purchaseController.getPurchaseRecords);
+router.get('/view_purchase_details/:lo', isPrivate, purchaseController.viewPurchaseDetails);
 
-//router.get('/view_purchase_records', isPurchasing, purchaseController.getAllPurchases);
-router.get('/create_purchase', isPurchasing, purchaseController.getCreatePurchase);
-router.post('/create_purchase', isPurchasing, purchaseController.postCreatePurchase);
-router.get('/get_price', isPurchasing, purchaseController.getPrice);
+//router.get('/view_purchase_records', isPrivate, purchaseController.getAllPurchases);
+router.get('/create_purchase', isPrivate, purchaseController.getCreatePurchase);
+router.post('/create_purchase', isPrivate, purchaseController.postCreatePurchase);
+router.get('/get_price', isPrivate, purchaseController.getPrice);
 //Logistics
-router.get('/schedule_delivery', isLogistics, deliveryController.scheduleDelivery);
-router.post('/schedule_delivery', isLogistics, deliveryController.updateDelivery);
-router.get('/ajaxChangeDeliveryInfo', isLogistics, deliveryController.getSingleDeliveryInfo);
-router.get('/ajaxChangeDriver', isLogistics, deliveryController.ajaxChangeDriver);
-router.get('/confirm_delivery', isLogistics, deliveryController.getConfirmDelivery);
-router.post('/confirm_delivery', isLogistics, deliveryController.updateDelivery);
-router.get('/track_deliveries', isLogistics, deliveryController.getTrackDelivery);
-router.get('/changeLogisticCalendar', isLogistics, deliveryController.changeCalendar);
-router.get('/view_delivery_records', isLogistics, deliveryController.getDeliveryRecords);
-router.get('/view_delivery_details/:id', isLogistics, deliveryController.viewDeliveryDetails);
+router.get('/schedule_delivery', isPrivate, deliveryController.scheduleDelivery);
+router.post('/schedule_delivery', isPrivate, deliveryController.updateDelivery);
+router.get('/ajaxChangeDeliveryInfo', isPrivate, deliveryController.getSingleDeliveryInfo);
+router.get('/ajaxChangeDriver', isPrivate, deliveryController.ajaxChangeDriver);
+router.get('/confirm_delivery', isPrivate, deliveryController.getConfirmDelivery);
+router.post('/confirm_delivery', isPrivate, deliveryController.updateDelivery);
+router.get('/track_deliveries', isPrivate, deliveryController.getTrackDelivery);
+router.get('/changeLogisticCalendar', isPrivate, deliveryController.changeCalendar);
+router.get('/view_delivery_records', isPrivate, deliveryController.getDeliveryRecords);
+router.get('/view_delivery_details/:id', isPrivate, deliveryController.viewDeliveryDetails);
 
 module.exports = router;
